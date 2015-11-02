941c95b5fd4c9617df8153bfb2150e3b578adf13
Matthew Butt <matthew.y.butt@gmail.com>
2015-11-01 16:34:56 -0800

    create sessions controller, route and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/login', to: 'sessions#new'
 end
