1191682760a1d9e0c13f4d77b010852c9ebedbc2
Matthew Butt <matthew.y.butt@gmail.com>
2015-11-04 19:15:36 -0800

    Add sortable date to Post index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
   before_action :authorize, except: [:index, :show]
 
+  helper_method :sort_column, :sort_direction
+
   def index
-    @posts = current_user.posts
+    @posts = current_user.posts.order(sort_column + " " + sort_direction)
   end
 
   def show
@@ -51,4 +53,13 @@ class PostsController < ApplicationController
     def post_params
       params.require(:post).permit(:title, :text, :image_url, :tag, :event_date, :user_id)
     end
+
+    def sort_column
+      current_user.posts.column_names.include?(params[:sort]) ? params[:sort] : "event_date"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+    end
+
 end
